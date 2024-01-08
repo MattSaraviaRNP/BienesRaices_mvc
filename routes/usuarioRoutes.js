0ee1ef36f2b75da0eb8327b8051f0a4798c4950b
MattSaraviaRNP <matt.saravia@rnp.hn>
2024-01-08 08:25:42 -0600

    Primer commit desde mi primera chambaaaa

---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const router = express.Router();
+
+
+router.get('/login', (req, res) => {
+    
+    res.render('auth/login', {
+        autenticado: true
+    })
+});
+
+
+
+
+export default router
